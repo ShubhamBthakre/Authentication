95fe496126a7a5eefb5e66bce76b24bd5f46a4b4
ShubhamBThakre <shubham.thakre31@gmail.com>
2023-11-06 11:39:21 +0000

    json web token mechanism

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
@@ -28,15 +29,35 @@ initializeDBAndServer();
 
 // Get Books API
 app.get("/books/", async (request, response) => {
-  const getBooksQuery = `
-  SELECT
-    *
-  FROM
-    book
-  ORDER BY
-    book_id;`;
-  const booksArray = await db.all(getBooksQuery);
-  response.send(booksArray);
+  let jwtToken;
+
+  const authHeader = request.headers["authorization"]; // Authorization=authorization
+
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+
+  if (authHeader === undefined) {
+    response.status(400);
+    response.send("Please provide access token");
+  } else {
+    //secret_key should be same provided as login API
+    jwt.verify(jwtToken, "secret_key", async (error, payload) => {
+      if (error) {
+        response.send("Invalid Access token");
+      } else {
+        const getBooksQuery = `
+                SELECT
+                    *
+                FROM
+                    book
+                ORDER BY
+                    book_id;`;
+        const booksArray = await db.all(getBooksQuery);
+        response.send(booksArray);
+      }
+    });
+  }
 });
 
 //Register User API
@@ -77,7 +98,11 @@ app.post("/login", async (request, response) => {
   } else {
     const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
     if (isPasswordMatched === true) {
-      response.send("Login Success!");
+      const payload = {
+        username: username,
+      };
+      const jwt_token = jwt.sign(payload, "secret_key"); //we can provide any value as secret key
+      response.send({ jwt_token });
     } else {
       response.status(400);
       response.send("Invalid Password");
